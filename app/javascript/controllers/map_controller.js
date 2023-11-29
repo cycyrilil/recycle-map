d388c1dcbe3d4e520369947c8113f4b3e3fb06b3
cyril Marek <marek.cyril@gmail.com>
2023-11-29 12:24:37 +0100

    corre seed + suite mapbox( pas de marker)

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,7 +16,9 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     });
-  }
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  };
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
@@ -25,5 +27,15 @@ export default class extends Controller {
         .setPopup(popup) // Add this
         .addTo(this.map)
     });
+  };
+
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
-}
+
+};
+
+
