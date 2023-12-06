74c8e977c2d1e64f571b57333ae4023e82643c2e
cyril Marek <marek.cyril@gmail.com>
2023-12-06 16:46:47 +0100

    debut de activity et profile

--- app/models/recycle.rb
+++ app/models/recycle.rb
@@ -2,6 +2,7 @@ class Recycle < ApplicationRecord
   belongs_to :place
   belongs_to :user
   has_many :recycle_categories, dependent: :destroy
+  has_many :categories, through: :recycle_categories
 
 
   def month
@@ -11,6 +12,10 @@ class Recycle < ApplicationRecord
 
   def year
     created_at.year
+  end
+
+  def day
+    created_at.day
   end
 
 end
